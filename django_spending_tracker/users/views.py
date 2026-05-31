8d61722850567d33c88083e260a5eb887be8ea5a
RichieG78 <richardglynn@gmail.com>
2026-05-31 15:58:23 +0100

    modified dashboard, new templates, login, logout, profile, register

--- django_spending_tracker/users/views.py
+++ django_spending_tracker/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from urllib import request
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm  #Import added
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) #UserRegisterForm instead of UserCreationForm
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully, now you can login.')
+        return redirect('login') 
+    else:
+        form = UserRegisterForm() #UserRegisterForm instead of UserCreationForm
+    return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
+
