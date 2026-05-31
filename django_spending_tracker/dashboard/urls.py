a9419fdfacfdc0c83031866c6f24b788af4c7bbc
RichieG78 <richardglynn@gmail.com>
2026-05-31 09:14:56 +0100

    templates and static for dashboard

--- django_spending_tracker/dashboard/urls.py
+++ django_spending_tracker/dashboard/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="dashboard"),
-    path("about/", views.about, name="dashboard_about")
+    path("", views.home, name="dashboard-home"),
+    path("about/", views.about, name="dashboard-about")
 ]
--- django_spending_tracker/dashboard/views.py
+++ django_spending_tracker/dashboard/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Steve K',
+        'title': 'First Blog Post',
+        'content': 'Content for the first post.',
+        'date_posted': '12/07/2023'
+    },
+
+     {
+        'author': 'Scott K',
+        'title': 'Second Blog Post',
+        'content': 'Content for the second post.',
+        'date_posted': '13/07/2023'
+    }
+
+] 
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the spending tracker index.")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'dashboard/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Spending Tracker - About</h1>')
+    return render(request, 'dashboard/about.html', {'title': 'About'})
+
