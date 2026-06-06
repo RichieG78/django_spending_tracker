c74d3b1c101d75c26657c6bf22e037ae30cfd27c
RichieG78 <richardglynn@gmail.com>
2026-06-06 08:21:00 +0100

    Password reset functionality

--- django_spending_tracker/dashboard/urls.py
+++ django_spending_tracker/dashboard/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     PostListView, 
     PostDetailView, 
@@ -14,5 +15,9 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'), #url for creating a new post, changed here
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), #url for updating a post, changed here
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), #url for deleting a post, changed here
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),	name="password_reset_confirm"),
+    path("reset/complete/", auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
     path('about/',views.about, name='dashboard-about'),
 ]
--- django_spending_tracker/django_spending_tracker/settings.py
+++ django_spending_tracker/django_spending_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,10 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = BASE_DIR / 'media' # Newer modern and Pythonic way 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True											#Transport Layer Security (like HTTP_S_]
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')			# app email address
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')	# app specific password
