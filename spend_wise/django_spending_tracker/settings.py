0097507f1db63a1815017831bf19434d85f2c87e
RichieG78 <richardglynn@gmail.com>
2026-06-13 11:12:06 +0100

    added default DATABASE_URL and Environment Variables

--- spend_wise/django_spending_tracker/settings.py
+++ spend_wise/django_spending_tracker/settings.py
@@ -93,6 +93,12 @@ DATABASES = {
     }
 }
 
+DATABASES = {									#ADDED
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
